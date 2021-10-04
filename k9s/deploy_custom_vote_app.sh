939166ee6c0b8242dfb8ebb26864469f4ac14d69
Miguel Julián <figarocorso@users.noreply.github.com>
2021-10-05 00:51:15 +0200

    Add K9s workshop slides (#1)
    * Initial k9s wireframe
    
    * Add deploy script example voting app
    
    * Make app fail and fix typo
    
    * Add most part of the workshop

---
+++ k9s/deploy_custom_vote_app.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+APP_MANIFESTS_TEMPLATE=example-voting-app.yaml.tpl
+APP_MANIFESTS=example-voting-app.yaml
+
+RANDOM_NUMBER=$(( 30000 + $RANDOM % 2766 ))
+
+NAMESPACE=vote${RANDOM_NUMBER}
+echo "Namespace: ${NAMESPACE}"
+RESULT_SERVICE_PORT=${RANDOM_NUMBER}
+echo "Results URL http://<IP>:${RESULT_SERVICE_PORT}"
+VOTE_SERVICE_PORT=$(( $RANDOM_NUMBER + 1 ))
+echo "Voting URL http://<IP>:${VOTE_SERVICE_PORT}"
+echo ""
+
+export NAMESPACE
+export RESULT_SERVICE_PORT
+export VOTE_SERVICE_PORT
+envsubst < ${APP_MANIFESTS_TEMPLATE} > ${APP_MANIFESTS}
+kubectl apply -f ${APP_MANIFESTS}
